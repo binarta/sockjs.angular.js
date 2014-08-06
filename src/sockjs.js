bd69706736cc5765605e46deb55f86ec19d6b257
wro <wro@thinkerit.be>
2014-08-06 14:08:39 +0200

    sockjs reconnects when socket gets closed

--- src/sockjs.js
+++ src/sockjs.js
@@ -1,15 +1,28 @@
 angular.module('binarta.sockjs', [])
-    .factory('sockJS', ['config', 'topicMessageDispatcher', SockJSFactory]);
+    .factory('sockJS', ['config', 'topicMessageDispatcher', 'topicRegistry', SockJSFactory]);
 
-function SockJSFactory(config, topicMessageDispatcher) {
-    var sock = SockJS(config.socketUri);
-    sock.onopen = function() {
-        topicMessageDispatcher.firePersistently('sockjs.loaded', 'ok');
+function SockJSFactory(config, topicMessageDispatcher, topicRegistry) {
+    var sock = undefined;
+
+    var init = function() {
+        sock = SockJS(config.socketUri);
+
+        sock.onopen = function() {
+            topicMessageDispatcher.firePersistently('sockjs.loaded', 'ok');
+        };
+        sock.onmessage = function(message) {
+            var data = JSON.parse(message.data);
+            topicMessageDispatcher.fire(data.topic, data.payload);
+        };
+        sock.onclose = function() {
+            init();
+        }
     };
-    sock.onmessage = function(message) {
-        var data = JSON.parse(message.data);
-        topicMessageDispatcher.fire(data.topic, data.payload);
-    };
+
+
+    topicRegistry.subscribe('config.initialized', function (config) {
+        init();
+    });
     return {
         send: function(data) {
             sock.send(JSON.stringify(data));
--- test/sockjs.spec.js
+++ test/sockjs.spec.js
@@ -4,40 +4,54 @@ describe('sockjs.js', function() {
     var config;
     var sut;
     var topicMessageDispatcherMock;
+    var topicRegistryMock;
 
     beforeEach(module('config'));
     beforeEach(module('notifications'));
     beforeEach(module('binarta.sockjs'));
 
-    beforeEach(inject(function(_config_, _topicMessageDispatcherMock_) {
+    beforeEach(inject(function(_config_, _topicMessageDispatcherMock_, _topicRegistryMock_) {
         config = _config_;
         topicMessageDispatcherMock = _topicMessageDispatcherMock_;
+        topicRegistryMock = _topicRegistryMock_;
     }));
 
     describe('sockJS', function() {
-       beforeEach(inject(function(topicMessageDispatcher) {
+       beforeEach(inject(function(topicMessageDispatcher, sockJS) {
            config.socketUri = 'http://localhost:8888/';
-           sut = SockJSFactory(config, topicMessageDispatcher);
+           sut = sockJS;
        }));
 
-        it('socket uri is passed', function() {
-            expect(_sock.url).toEqual(config.socketUri);
-        });
+        describe('on config.initialized', function() {
+            beforeEach(function() {
+                topicRegistryMock['config.initialized'](config);
+            });
 
-        it('on open fire sockjs.loaded', function() {
-            _sock.onopen();
-            expect(topicMessageDispatcherMock.persistent['sockjs.loaded']).toEqual('ok');
-        });
+            it('socket uri is passed', function() {
+                expect(_sock.url).toEqual(config.socketUri);
+            });
 
-        it('on message fire notification for response address', function() {
-            _sock.onmessage({data: JSON.stringify({topic:'T', payload:'P'})});
-            expect(topicMessageDispatcherMock['T']).toEqual('P');
-        });
+            it('on open fire sockjs.loaded', function() {
+                _sock.onopen();
+                expect(topicMessageDispatcherMock.persistent['sockjs.loaded']).toEqual('ok');
+            });
 
-        it('send data over socket', function() {
-            sut.send({payload:'P'});
-            expect(_sock.data).toEqual(JSON.stringify({payload:'P'}));
-        })
+            it('on message fire notification for response address', function() {
+                _sock.onmessage({data: JSON.stringify({topic:'T', payload:'P'})});
+                expect(topicMessageDispatcherMock['T']).toEqual('P');
+            });
+
+            it('send data over socket', function() {
+                sut.send({payload:'P'});
+                expect(_sock.data).toEqual(JSON.stringify({payload:'P'}));
+            });
+
+            it('on close re init socket', function() {
+                var previous = _sock;
+                _sock.onclose();
+                expect(_sock).toNotEqual(previous);
+            })
+        });
     });
 
 });
